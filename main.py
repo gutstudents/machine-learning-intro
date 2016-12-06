266c2e395c4503475febd20b085646877fd2bf89
lmiadowicz <l.miadowicz@gmail.com>
2016-12-06 22:08:48 +0100

    Added conversion functions

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from csv import reader
 
 def load_csv(filename):
-    file = open(filename,"r")
-    lines = reader(file)
-    dataset = list(lines)
+    dataset = list()
+    with open(filename,"r") as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
     return dataset
+
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+def str_column_to_int():
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = 1
+    for row in dataset:
+        row[column] = lookup[row[column]]
+        return lookup
 
 
 # Load dataset
-filename = 'pima-indians-diabetes.csv'
+filename = './pima-indians-diabetes.csv'
 dataset = load_csv(filename)
 print('Loaded data file {0} with {1} rows and {2} columns.'.format(filename,len(dataset),len(dataset[0])))
 
+for i in range(len(dataset[0])):
+    str_column_to_float(dataset,i)
+print(dataset[9])
